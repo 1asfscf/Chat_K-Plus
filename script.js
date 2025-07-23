dd0c540380fefdb6304d329a7139434c2913d51f
1asfscf <lieolkingta@gmail.com>
2025-07-24 02:01:39 +0900

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// script.js
 const loginBtn = document.querySelector('.login-btn');
 const modal    = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close-btn');
 
-loginBtn.addEventListener('click', () => {
-  modal.classList.remove('hidden');
-});
-closeBtn.addEventListener('click', () => {
-  modal.classList.add('hidden');
-});
+loginBtn.addEventListener('click', () => modal.classList.remove('hidden'));
+closeBtn.addEventListener('click', () => modal.classList.add('hidden'));
